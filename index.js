a397ad38e8f6c77f8c39d673a8f14a3b338e2483
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-02-09 21:46:56 -0500

    display tasks and total amounts

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 import { jobs } from "./data.js";
+
+const tasksEl = document.getElementById("tasks");
+const tasksSelected = document.getElementById("tasks-selected");
+const totalAmount = document.getElementById("total-amount");
+const tasksArray = [];
+const tasksPrices = [];
+
+jobs.forEach(function ({ name, price, id }) {
+  tasksEl.innerHTML += `
+    <button class="task" data-add="${id}">${name} $${price}</button>
+    `;
+});
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.add) {
+    tasksArray.push(jobs[e.target.dataset.add]);
+    tasksPrices.push(jobs[e.target.dataset.add].price);
+    showSelectedTasks(tasksArray);
+    showTotalAmount(tasksPrices);
+  }
+});
+
+function showSelectedTasks(arr) {
+  let taskHtml = "";
+
+  arr.forEach(function ({ name, price }) {
+    taskHtml += `
+    <div>
+        <p>${name}</p>
+        <p>$${price}</p>
+    </div>`;
+  });
+
+  tasksSelected.innerHTML = taskHtml;
+}
+
+function showTotalAmount(arr) {
+  let total = arr.reduce((a, b) => a + b, 0);
+
+  totalAmount.innerHTML = `$${total}`;
+  console.log(total);
+}
