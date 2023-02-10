aedad397c823df12df0051bc8fb8504f4a78d08f
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-02-10 18:37:13 -0500

    remove closest div when clicking on remove btn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const tasksEl = document.getElementById("tasks");
 const tasksSelected = document.getElementById("tasks-selected");
 const totalAmount = document.getElementById("total-amount");
 const tasksArray = [];
-const tasksPrices = [];
+let tasksPrices = [];
 
 jobs.forEach(function ({ name, price, id }) {
   tasksEl.innerHTML += `
@@ -13,22 +13,33 @@ jobs.forEach(function ({ name, price, id }) {
 });
 
 document.addEventListener("click", function (e) {
-  if (!tasksArray.includes(jobs[e.target.dataset.add])) {
+  if (
+    e.target.dataset.add &&
+    !tasksArray.includes(jobs[e.target.dataset.add])
+  ) {
     tasksArray.push(jobs[e.target.dataset.add]);
     tasksPrices.push(jobs[e.target.dataset.add].price);
     showSelectedTasks(tasksArray);
     showTotalAmount(tasksPrices);
-    console.log(jobs[e.target.dataset.add]);
+  }
+  if (e.target.dataset.remove) {
+    let removedItem = e.target.dataset.remove;
+    let itemIndex = tasksArray.findIndex((item) => item.id == removedItem);
+    tasksArray.splice(itemIndex, 1);
+    console.log(tasksArray);
+    tasksPrices = tasksPrices.filter((price, index) => index != itemIndex);
+    e.target.closest("div").remove();
+    showTotalAmount(tasksPrices);
   }
 });
 
 function showSelectedTasks(arr) {
   let taskHtml = "";
 
-  arr.forEach(function ({ name, price }) {
+  arr.forEach(function ({ name, price, id }) {
     taskHtml += `
     <div>
-        <p>${name}</p>
+        <p>${name} <button data-remove= "${id}">remove</button></p>
         <p>$${price}</p>
     </div>`;
   });
